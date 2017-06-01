6609754e5a8e21d4178a9870fb9f163a14d8f74b
merkjinx <merkjinx@users.noreply.github.com>
2017-06-01 12:38:56 -0700

    Add files via upload

---
+++ Windowslocker/Windowslocker/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windowslocker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            this.TopMost = true;
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.KeyPreview = true;
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+
+           // if (this.WindowState == FormWindowState.Normal)
+            //{
+             //   this.WindowState = FormWindowState.Maximized;
+            //}
+
+            //this.Activate();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_altF4Pressed)
+            {
+                if (e.CloseReason == CloseReason.UserClosing)
+                    e.Cancel = true;
+                _altF4Pressed = false;
+            }
+        }
+
+        private bool _altF4Pressed;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Alt && e.KeyCode == Keys.F4)
+                _altF4Pressed = true;
+        }
+    }
+}
---
+++ Windowslocker/Windowslocker/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windowslocker
+{
+    static class Program
+    {
+        /// <summary>Returns true if the current application has focus, false otherwise</summary>
+        public static bool ApplicationIsActivated()
+        {
+            var activatedHandle = GetForegroundWindow();
+            if (activatedHandle == IntPtr.Zero)
+            {
+                return false;       // No window is currently activated
+            }
+
+            var procId = Process.GetCurrentProcess().Id;
+            int activeProcId;
+            GetWindowThreadProcessId(activatedHandle, out activeProcId);
+
+            return activeProcId == procId;
+        }
+
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true)]
+        private static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern int GetWindowThreadProcessId(IntPtr handle, out int processId);
+        static void checker()
+        {
+
+            if(ApplicationIsActivated())
+            {
+                Console.WriteLine(false);
+            }
+            System.Threading.Thread.Sleep(1000);
+        }
+        private static void tbN1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            String sKeys = "1234567890ABCDEF";
+            if (!sKeys.Contains(e.KeyChar.ToString().ToUpper()))
+                e.Handled = true;
+        }
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Thread t = new Thread(checker);
+            t.Start();
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+        }
+    }
+}
